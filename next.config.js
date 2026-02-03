3126df1b5fbc1f0f19e62a9d8dba8e8332861fa6
tcr99001-ctrl <tcr99001@gmail.com>
2026-02-03 10:38:58 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,4 @@
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // 1. 서버 전용 패키지(undici)가 브라우저 번들에 포함되지 않도록 'alias'로 막습니다.
@@ -10,7 +11,7 @@ const nextConfig = {
     }
     return config;
   },
-  // 2. 혹시 모르니 트랜스파일 설정도 유지합니다.
+  // 2. 혹시 모르니 트랜스파일 설정도 유지합니다...
   transpilePackages: ['undici', 'firebase', '@firebase/auth'],
 };
 
